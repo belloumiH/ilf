db0777196e974693b446b487dedf01b45cbe0a07
root <root@Symfony.ip-94-23-237.eu>
2020-03-07 19:13:58 +0300

    alba

--- src/Helper/Mail.php
+++ src/Helper/Mail.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Helper;
 
-use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
@@ -13,48 +12,28 @@ use PHPMailer\PHPMailer\SMTP;
  */
 class Mail
 {
-    /**
-     * @return bool
-     */
-    public static function send()
+    public function send($data)
     {
-        $mail = new PHPMailer(true);
-        try {
-            //Server settings
-            $mail->isSMTP();                                            // Send using SMTP
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-            $mail->Host = 'smtp.gmail.com';                       // Set the SMTP server to send through
-            $mail->SMTPAuth = true;                                   // Enable SMTP authentication
-            $mail->Username = 'consulting.ilf@gmail.com';             // SMTP username
-            $mail->Password = 'R3Fk2bU2wLk32uG';                      // SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-            $mail->Port = 25;                                    // TCP port to connect to
+        $mail = new PHPMailer();  // Cree un nouvel objet PHPMailer
+    // $mail->IsSMTP(); // active SMTP
+    $mail->SMTPDebug = 3;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
+    $mail->SMTPAuth = true;  // Authentification SMTP active
+    $mail->SMTPSecure = 'ssl'; // Gmail REQUIERT Le transfert securise
+    $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 465;
+        $mail->Username = 'oussema.albouchii@gmail.com';
+        $mail->Password = '21877316azertyzazerter.97';
+        $mail->SetFrom('oussema.albouchii@gmail.com', 'ilf');
+        $mail->Subject = 'test';
+        $mail->Body = 'this is a mail test';
+        $mail->AddAddress('idrisshosni@ilfconsulting.fr');
+        if (!$mail->Send()) {
+            var_dump($mail->ErrorInfo);
+            die;
 
-            //Recipients
-            $mail->setFrom('contact@ilfconsulting.fr', 'Ilf-consulting');
-//            $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-            $mail->addAddress('haythem.belloumi@gmail.com');               // Name is optional
-//            $mail->addReplyTo('info@example.com', 'Information');
-//            $mail->addCC('cc@example.com');
-//            $mail->addBCC('bcc@example.com');
-
-            // Attachments
-//            $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-            // Content
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-            $mail->send();
-            echo 'Mailer Error: '.$mail->ErrorInfo;
-
+            return 'Mail error: '.$mail->ErrorInfo;
+        } else {
             return true;
-        } catch (Exception $e) {
-            return false;
-//            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
     }
 }
